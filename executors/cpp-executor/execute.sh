0b12ca995045beac763700ada323da857c8020b7
arjun-s <arjun-s@arjun-s.local>
2025-02-18 03:11:35 +0530

    Handled input for c++

--- executors/cpp-executor/execute.sh
+++ executors/cpp-executor/execute.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Create a C++ file inside the container 
+echo -e "$CODE" | base64 --decode > /usr/src/app/main.cpp
+
+# Create input file inside the container (only if input is provided)
+if [ ! -z "$INPUT" ]; then
+    echo -e "$INPUT" | base64 --decode > /usr/src/app/input.txt
+fi
+
 # Check if the C++ source file exists
 if [ ! -f "/usr/src/app/main.cpp" ]; then
   echo "No C++ code found!"
@@ -14,7 +22,12 @@ if [ $? -ne 0 ]; then
 fi
 
 # Run the compiled program and capture runtime errors
-/usr/src/app/main.out 2> runtime_error.txt
+if [ -f "/usr/src/app/input.txt" ]; then
+    /usr/src/app/main.out < /usr/src/app/input.txt 2>&1 | tee runtime_error.txt
+else
+    /usr/src/app/main.out 2>&1 | tee runtime_error.txt
+fi
+
 if [ $? -ne 0 ]; then
   cat runtime_error.txt  # Print runtime errors
   exit 1
